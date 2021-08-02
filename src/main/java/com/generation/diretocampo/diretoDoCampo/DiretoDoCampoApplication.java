2905a33ebd004f8644b853c3b6e48578a40a0ee7
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-02 15:02:40 -0300

    first commit

---
+++ src/main/java/com/generation/diretocampo/diretoDoCampo/DiretoDoCampoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.diretocampo.diretoDoCampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiretoDoCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiretoDoCampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/diretocampo/diretoDoCampo/DiretoDoCampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.diretocampo.diretoDoCampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiretoDoCampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
